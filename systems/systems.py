4d45b442a3171372ddc592672ed2eef9077994d9
RikTU <t.h.huijzer@student.tue.nl>
2018-10-13 08:56:37 +0200

    rasa classification works after refactor

--- systems/systems.py
+++ systems/systems.py
@@ -1,19 +1,17 @@
 from abc import ABC, abstractmethod
-from pathlib import Path
 
 import pandas
 import rasa_nlu.training_data
 from deeppavlov.agents.default_agent.default_agent import DefaultAgent
 from deeppavlov.agents.processors.highest_confidence_selector import HighestConfidenceSelector
 from deeppavlov.skills.pattern_matching_skill import PatternMatchingSkill
-from rasa_nlu import config
 from rasa_nlu.model import Interpreter
 from rasa_nlu.model import Trainer
 from rasa_nlu.training_data import TrainingData
 from rasa_nlu.training_data import Message
-from spacy.errors import Errors
-import spacy
-import subprocess
+from rasa_nlu.config import RasaNLUModelConfig
+import sklearn
+import warnings
 
 
 class System(ABC):
@@ -30,43 +28,42 @@ class System(ABC):
 
 
 class Rasa(System):
+    # For any intent having only one utterance it is common that sklearn gives the following warning:
+    # UndefinedMetricWarning: F-score is ill-defined and being set to 0.0 in labels with no predicted samples.
+    # this is no big issue according to Tom Bocklisch: https://github.com/RasaHQ/rasa_nlu/issues/288
+    # training_file = rasa_nlu.training_data.loading._load(self.converter.training_file)
+    warnings.filterwarnings("ignore", category=sklearn.exceptions.UndefinedMetricWarning)
+
     interpreter: rasa_nlu.model.Interpreter
 
     def __init__(self, train: pandas.DataFrame):
         super().__init__(train)
 
-    def train_default(self):  # TODO: Update this
-        # message = Message('sentence', data={'intent': 'some intent'})
-        # message = Message.build('sentence', 'intent')
+    def train_default(self):
+        configuration_values = {
+            'language': 'en_core_web_sm',
+            'pipeline': [{'name': 'nlp_spacy'},
+                         {'name': 'tokenizer_spacy'},
+                         {'name': 'ner_crf'},
+                         {'name': 'intent_featurizer_spacy'},
+                         {'name': 'intent_classifier_sklearn'},
+                         {'name': 'ner_synonyms'}],
+            'data': None
+        }
+        trainer = Trainer(RasaNLUModelConfig(configuration_values))
+
         training_examples = []
         for _, row in self.train.iterrows():
             training_examples.append(Message.build(row['sentence'], row['intent']))
-
         training_data = TrainingData(training_examples=training_examples)
 
-        # For any intent having only one utterance it is common that sklearn gives the following warning:
-        # UndefinedMetricWarning: F-score is ill-defined and being set to 0.0 in labels with no predicted samples.
-        # this is no big issue according to Tom Bocklisch: https://github.com/RasaHQ/rasa_nlu/issues/288
-        # training_file = rasa_nlu.training_data.loading._load(self.converter.training_file)
-
-        # TODO: Not using config file but RasaNLUModelConfig
-        # TODO: Use Spacy loading model method described below
-        # import en_core_web_sm
-        # nlp = en_core_web_sm.load()
-        try:
-            spacy.util.load_model('en_core_web_sm')
-        except IOError:
-            print('Spacy language model appears to be missing. Attempting to install...')
-            subprocess.call(['python', '-m', 'spacy', 'download', 'en'])
-
-        trainer = Trainer(config.load(Path(__file__).parent / 'rasa' / 'config.yml'))
         self.interpreter = trainer.train(training_data)
 
     def _get_classification(self, sentence):
         return self.interpreter.parse(sentence)
 
     def get_intent(self, sentence):
-        return self._get_classification(sentence)['intent']
+        return self._get_classification(sentence)['intent']['name']
 
 
 class DeepPavlov(System):
